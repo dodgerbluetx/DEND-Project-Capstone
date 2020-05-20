964bd5955dee59b83e0ec7c01c9b855c88abbdb7
Alvarez, Mark <dodgerbluetx@gmail.com>
2020-05-20 08:52:36 +0000

    working etl using s3 csv

--- etl.py
+++ etl.py
@@ -8,19 +8,21 @@ from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, dat
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType, TimestampType, LongType, DecimalType
 
 config = configparser.ConfigParser()
-config.read_file(open('dl.cfg'))
+config.read_file(open('/home/hadoop/dl.cfg'))
 
 os.environ["AWS_ACCESS_KEY_ID"]= config['AWS']['AWS_ACCESS_KEY_ID']
 os.environ["AWS_SECRET_ACCESS_KEY"]= config['AWS']['AWS_SECRET_ACCESS_KEY']
 
 spark = SparkSession \
     .builder \
-    .config("spark.jars.packages", "saurfang:spark-sas7bdat:2.0.0-s_2.11,org.apache.hadoop:hadoop-aws:2.7.0") \
+    .config("spark.jars.packages", "saurfang:spark-sas7bdat:2.1.0-s_2.11,org.apache.hadoop:hadoop-aws:2.7.0") \
     .enableHiveSupport() \
     .getOrCreate()
 
 #df_spark = spark.read.format('com.github.saurfang.sas.spark').option("inferSchema", "true").load('i94_feb16_sub.sas7bdat')
-df_spark = spark.read.csv('immigration_data_sample.csv', header=True, inferSchema=True)
+#df_spark = spark.read.csv('/home/hadoop/immigration_data_sample.csv', header=True, inferSchema=True)
+#df_spark = spark.read.csv('s3a://ma2516-immigration-data/immigration_data_sample.csv', header=True, inferSchema=True)
+df_spark = spark.read.format('com.github.saurfang.sas.spark').option("inferSchema", "true").load('s3a://ma2516-immigration-data/i94_feb16_sub.sas7bdat')
 
 df_spark.show(5)
 df_spark.printSchema()
@@ -40,4 +42,5 @@ immigration_table = spark.sql('''
 ''')
 immigration_table.show()
 
-immigration_table.write.mode("overwrite").partitionBy("i94port").parquet("s3a://ma2516-immigration-table/")
+#immigration_table.write.mode("overwrite").partitionBy("i94port").parquet("s3a://ma2516-immigration-table/")
+immigration_table.write.mode("overwrite").partitionBy("i94port").parquet("s3a://ma2516-immigration-table/data")
